b35178ab64f1c4266aa31ae8aae3ed46e0506af8
Prabeen <prabeensoreng2119@gmail.com>
2018-12-04 21:50:21 +0530

    finished adding and displaying todos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,26 @@
 import { Component } from '@angular/core';
+import { Todo } from './class/todo';
+import { TodoDataService } from './services/todo-data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent {
+  newTodo = new Todo();
+
+  constructor(private todoService: TodoDataService) {}
+
+  get todos() {
+    return this.todoService.getAllTodos();
+  }
+
+  addTodo() {
+    if(this.newTodo.title && this.newTodo.date) {
+      this.todoService.addTodo(this.newTodo);
+      this.newTodo.title = '';
+      this.newTodo.date = '';
+    }
+  }
+}
--- src/app/services/todo-data.service.ts
+++ src/app/services/todo-data.service.ts
@@ -10,9 +10,23 @@ export class TodoDataService {
 
   constructor() { }
 
+  addTodo(todo: Todo) {
+    if(!todo.id) {
+      todo.id = ++this.lastId;
+    }
+    const todos = this.getAllTodos();
+    todos.push(todo);
+    this.setTodo(todos);
+    return this;
+  }
+
   getAllTodos(): Todo[] {
     const storageItem = JSON.parse(window.localStorage.getItem('app-todos'));
     if(storageItem === null) return [];
     else return storageItem.todos;
+  }
+
+  setTodo(todos: Todo[]) {
+    window.localStorage.setItem('app-todos', JSON.stringify({todos: todos}));
   }
 }
